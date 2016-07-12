5e9794899b4c225370eed6e5aee376ffe659f3d9
Olivia Ross <rossolivie1@gmail.com>
2016-07-12 18:04:52 -0400

    Added tilemap for level one and got the level set-up started. Soon we should be able to add new levels in JSON really quickly, which is nice.

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,27 +1,19 @@
-// Create the state that will contain the whole game
-var mainState = {
-   preload: function() {
-      // Here we preload the assets
-      game.load.image('player', 'assets/magicalgirl1.png');
-      game.load.image('background', 'assets/background.jpg');
+var Menu = {
+   preload : function() {
+        // Loading images is required so that later on we can create sprites based on the them.
+        // The first argument is how our image will be refered to, 
+        // the second one is the path to our file.
+        game.load.image('menu', './assets/img/mainmenu.png');
+    },
 
-   },
+    create: function () {
+        // Add a sprite to your game, here the sprite will be the game's logo
+        // Parameters are : X , Y , image name (see above) 
+        this.add.button(0, 0, 'menu',this.startGame, this);
+    },
 
-   create: function() {
-      // Here we create the game
-
-      // Set the background color to blue
-      game.add.sprite(0, 0, 'background');
-
-
-   },
-
-   update: function() {
-      // Here we update the game 60 times per second
-   },
-};
-
-// Initialize the game and start our state
-var game = new Phaser.Game(800, 800);
-game.state.add('main', mainState);
-game.state.start('main');
+    startGame: function() {
+      game.state.add ('Game', Game);
+      this.state.start('Game');
+    }
+ };    
