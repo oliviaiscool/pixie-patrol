7c874c01a16003cc1fc0641888564cd3f4956b83
olivia ross! <ross.mollie@gmail.com>
2016-07-16 21:46:47 -0400

    Delete main.js

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-var game;
-
-// create new game instance that's 800px wide and 800px tall
-game = new Phaser.Game(800,800, Phaser.AUTO, '');
-
-// add menu game state
-game.state.add ('Menu', Menu);
-game.state.start ('Menu');
